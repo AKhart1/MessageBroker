b3a4418c7739f668c3ac91817c2323ed22b45149
Alex Khartaniuk <S200823@sggw.edu.pl>
2023-12-17 17:25:43 +0100

    Added Web API project with gitignore

---
+++ MessageBroker/Models/Subscription.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessageBroker.Models
+{
+    public class Subscription
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public int TopicId { get; set; }
+    }
+}
